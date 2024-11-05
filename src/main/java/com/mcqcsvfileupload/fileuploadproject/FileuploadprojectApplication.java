4eb235b1854261b781cb8755a184b7508131e85a
Hp <thakre.atul26@gmail.com>
2024-11-05 14:01:30 +0530

    Initial Commit

---
+++ src/main/java/com/mcqcsvfileupload/fileuploadproject/FileuploadprojectApplication.java
@@ -0,0 +1,13 @@
+package com.mcqcsvfileupload.fileuploadproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileuploadprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileuploadprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcqcsvfileupload/fileuploadproject/FileuploadprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcqcsvfileupload.fileuploadproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileuploadprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
